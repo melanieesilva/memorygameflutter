6d841e857941eedff7dcbe838bd2a9c094a4af57
Melanie <melaine.santos@ba.estudante.senai.br>
2023-12-04 15:46:59 -0300

    Adiciona constante de cores

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const Color background = Color(0xFF202838);
+  static const Color inputBack = Color(0xFF2C374A);
+  static const Color border = Color(0xFF3C4A63);
+  static const Color subtitle = Color(0xFF798CAE);
+  static const Color backCard = Color(0xFFB4CEFF);
+  static const Color btnColor = Color(0xFF004CDD);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memory/common/constants/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Olá", textDirection: TextDirection.ltr,),
+      decoration: const BoxDecoration(
+        color: AppColors.background
+      ),
+      child: const Center(
+        child: Text(
+          "Memory Game",
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+            fontFamily:'KronaOne',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
     );
   }
 }
