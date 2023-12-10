a94534e91711d54cab40158b12f5e86e1c9b4762
Isabel Costa <125329797+zabeldsc@users.noreply.github.com>
2023-12-10 19:40:32 -0300

    Imagens aparecem

--- lib/gameScreen.dart
+++ lib/gameScreen.dart
@@ -23,74 +23,48 @@ class _GameScreenState extends State<GameScreen> {
   ];
 
   List<bool> flippedCards = List.filled(16, false);
+  int misses = 0;
+  int hits = 0;
 
-  List<Widget> buildCardWidgets() {
-     List<String> allCards = [...cardValues, ...cardValues];
-
-    return List.generate(allCards.length, (index) {
-      return buildCard(index, allCards[index]);
-    });
+  List<String> buildShuffledCards() {
+    List<String> allCards = [...cardValues, ...cardValues];
+    allCards.shuffle();
+    return allCards;
   }
 
   Widget buildCard(int index, String cardValue) {
     return GestureDetector(
       onTap: () {
-        // Handle card click
         setState(() {
           flippedCards[index] = !flippedCards[index];
         });
       },
       child: Container(
-        width: 68,
-        height: 100,
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              child: Container(
-                width: 68,
-                height: 100,
-                decoration: ShapeDecoration(
-                  color: Color(0xFF3C4A63),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 4,
-              top: 46,
-              child: SizedBox(
-                width: 60,
-                child: Text(
-                  'MYMG',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFFB4CEFF),
-                    fontSize: 10,
-                    fontFamily: 'Krona One',
-                    fontWeight: FontWeight.w400,
-                    height: 0,
-                  ),
-                ),
-              ),
-            ),
-            if (flippedCards[index])
-              Positioned(
-                left: 24,
-                top: 24,
-                child: Image.asset(
-                  'assets/$cardValue',
-                  width: 24,
-                  height: 24,
-                ),
-              ),
-          ],
+        width: 80,
+        height: 120,
+        decoration: BoxDecoration(
+          color: Color(0xFF3C4A63),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: EdgeInsets.all(4),
+        child: Center(
+          child: flippedCards[index]
+              ? Image.asset(
+                  'assets/images/$cardValue',
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.contain,
+                )
+              : Container(), // Oculta a imagem se não estiver virada
         ),
       ),
     );
+  }
+
+  List<Widget> buildCardWidgets(List<String> shuffledCards) {
+    return List.generate(shuffledCards.length, (index) {
+      return buildCard(index, shuffledCards[index]);
+    });
   }
 
   @override
@@ -99,305 +73,90 @@ class _GameScreenState extends State<GameScreen> {
         ? widget.playerNames[0]
         : 'Nome do Jogador';
 
-    return Container(
-      width: 360,
-      height: 800,
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(color: Color(0xFF202838)),
-      child: Stack(
-        children: [
-          Positioned(
-            left: 103,
-            top: 40,
-            child: SizedBox(
-              width: 153,
-              child: Text(
-                'Memory Game',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.08,
-                  fontFamily: 'Krona One',
-                  fontWeight: FontWeight.w400,
-                  height: 0,
-                  decoration: TextDecoration.none,
-                ),
+    List<String> shuffledCards = buildShuffledCards();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Memory Game'),
+        centerTitle: true,
+        backgroundColor: Color(0xFF202838),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'One Player - $playerName',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          Positioned(
-            left: (360 - 153) / 2, // Posiciona horizontalmente para centralizar
-            top: 94,
-            child: Container(
-              alignment: Alignment.center, // Alinha o conteúdo ao centro
-              child: SizedBox(
-                width: 153,
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'One Player - ',
-                        style: TextStyle(
-                          color: Color(0xFF6EA0FF),
-                          fontSize: 10,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w500,
-                          height: 0,
-                          decoration: TextDecoration.none,
-                        ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Misses',
+                      style: TextStyle(
+                        color: Color(0xFFFF4D4D),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
                       ),
-                      TextSpan(
-                        text: playerName,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 10,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w700,
-                          height: 0,
-                          decoration: TextDecoration.none,
-                        ),
+                    ),
+                    Text(
+                      misses.toString(),
+                      style: TextStyle(
+                        color: Color(0xFFFF4D4D),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
                       ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 69,
-            top: 163,
-            child: Container(
-              width: 105,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              decoration: ShapeDecoration(
-                color: Color(0xFF2C374A),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                Column(
+                  children: [
+                    Text(
+                      'Hits',
+                      style: TextStyle(
+                        color: Color(0xFF38F2A4),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      hits.toString(),
+                      style: TextStyle(
+                        color: Color(0xFF38F2A4),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Misses',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFFFF4D4D),
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  const SizedBox(width: 22),
-                  Text(
-                    '0',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFFFF4D4D),
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                ],
-              ),
+              ],
             ),
-          ),
-          Positioned(
-            left: 186,
-            top: 163,
-            child: Container(
-              width: 105,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              decoration: ShapeDecoration(
-                color: Color(0xFF2C374A),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Hits',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFF38F2A4),
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  const SizedBox(width: 22),
-                  Text(
-                    '0',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color(0xFF38F2A4),
-                      fontSize: 14,
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w500,
-                      height: 0,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            left: 25,
-            top: 40,
-            child: Container(
-              width: 24,
-              height: 24,
-              clipBehavior: Clip.antiAlias,
-              decoration: BoxDecoration(),
-            ),
-          ),
-          Positioned(
-            left: 24,
-            top: 228,
-            child: Container(
-              width: 312,
-              height: 400, // Altura suficiente para exibir todas as cartas
+            SizedBox(height: 16),
+            Expanded(
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4, // Número de colunas
-                  crossAxisSpacing: 8.0, // Espaçamento entre colunas
-                  mainAxisSpacing: 8.0, // Espaçamento entre linhas
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
                 ),
-                itemCount: buildCardWidgets().length,
+                itemCount: shuffledCards.length,
                 itemBuilder: (context, index) {
-                  return buildCardWidgets()[index];
+                  return buildCard(index, shuffledCards[index]);
                 },
               ),
             ),
-          ),
-          Positioned(
-            left: 24,
-            top: 712,
-            child: Container(
-              width: 312,
-              padding: const EdgeInsets.symmetric(horizontal: 28),
-              clipBehavior: Clip.antiAlias,
-              decoration: ShapeDecoration(
-                color: Color(0xFF2C374A),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 24,
-                          height: 24,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 24),
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 24,
-                          height: 24,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 2.86,
-                                top: 2.85,
-                                child: Container(
-                                  width: 18.29,
-                                  height: 18.29,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 24),
-                  Container(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 24,
-                          height: 24,
-                          clipBehavior: Clip.antiAlias,
-                          decoration: BoxDecoration(),
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Text(
-                          'Timer',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0xFFB4CEFF),
-                            fontSize: 14,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w400,
-                            height: 0,
-                            decoration: TextDecoration.none,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
